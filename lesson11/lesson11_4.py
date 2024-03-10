50ab19acbc7c333c03b2001c4a3366ab62b80418
徐國堂 <roberthsu2003@gmail.com>
2024-03-10 03:57:44 +0000

    modify

---
+++ lesson11/lesson11_4.py
@@ -0,0 +1,24 @@
+import csv
+
+students = [{'國文': 90, '數學': 88, '英文': 61},
+            {'國文': 98, '數學': 85, '英文': 63},
+            {'國文': 86, '數學': 99, '英文': 85},
+            {'國文': 52, '數學': 52, '英文': 71},
+            {'國文': 53, '數學': 58, '英文': 98},
+            {'國文': 54, '數學': 66, '英文': 54},
+            {'國文': 94, '數學': 67, '英文': 70},
+            {'國文': 69, '數學': 87, '英文': 89},
+            {'國文': 100, '數學': 75, '英文': 100},
+            {'國文': 100, '數學': 100, '英文': 100}]
+
+
+
+
+#建立file實體,這個實體會自動close()-> with....as
+
+with open('students.csv',mode='w',encoding='utf-8',newline='') as csvfile:
+    writer = csv.DictWriter(csvfile,fieldnames=['國文','數學','英文'])
+    writer.writeheader()
+    writer.writerows(students)
+
+print('students.csv存檔完成')
