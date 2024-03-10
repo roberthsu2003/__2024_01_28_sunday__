a38b684a9e06f017978b122c57269ff3d022298d
徐國堂 <roberthsu2003@gmail.com>
2024-03-10 02:43:10 +0000

    modify

--- lesson11/lesson11_2.py
+++ lesson11/lesson11_2.py
@@ -2,11 +2,15 @@ import pyinputplus as pyip
 import random
 from pprint import pprint
 
+def get_student(n:int)->list[dict]:
+    students:list[dict] = []
+    for _ in range(n):
+        student:dict[str,int] = {}
+        for subject in ['國文','英文','數學']:
+            student[subject] = random.randint(50,100)
+        students.append(student)
+    return students
+
 nums = pyip.inputInt("請輸入學生數量:")
-students:list[dict] = []
-for _ in range(nums):
-    student:dict[str,int] = {}
-    for subject in ['國文','英文','數學']:
-        student[subject] = random.randint(50,100)
-    students.append(student)
-pprint(students)
+stus = get_student(n=nums)
+pprint(stus)
