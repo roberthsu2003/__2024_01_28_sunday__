a4576a5d4c7b36ff5d0c8748df557150cdf09319
徐國堂 <roberthsu2003@gmail.com>
2024-04-14 08:29:55 +0000

    modify

--- line_bot/index.py
+++ line_bot/index.py
@@ -25,6 +25,7 @@ def callback():
 def handle_message(event):
     message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
+    
 
 import os
 if __name__ == "__main__":
